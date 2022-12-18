1706888a2fd421962f05f7c87bb69f090085e0c1
Sabina Auhunas <110496702+Sabishka10101@users.noreply.github.com>
2022-12-18 12:05:54 +0100

    create file div-ghost

---
+++ script.js
@@ -0,0 +1,7 @@
+var id1 = document.getElementById("id1"), id2 = document.getElementById("id2");
+id1.onmouseover = function () {
+    id2.innerHTML = "<b>" + document.getElementById("txt").value + "</b>";
+}
+id1.onmouseout = function () {
+    id2.innerHTML = "";
+}
