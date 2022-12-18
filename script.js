38cabaa33f6dee5eada50a41700c126d42fe94b3
Sabina Auhunas <110496702+Sabishka10101@users.noreply.github.com>
2022-12-18 12:57:35 +0100

    change file

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var id1 = document.getElementById("id1"), id2 = document.getElementById("id2");
 id1.onmouseover = function () {
-    id2.innerHTML = "<b>" + document.getElementById("txt").value + "</b>";
+id2.innerHTML = "<b>" + document.getElementById("txt").value + "</b>";
 }
 id1.onmouseout = function () {
     id2.innerHTML = "";
